110ecfa4503ee9f274f49c1e07967731db749ba3
Maran <www.sent2manimaran@gmail.com>
2022-12-22 19:00:44 +0530

    before separating user and admin

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { urlencoded } = require('express');
 const express = require('express');
+const ejs = require('ejs');
 const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
 mongoose.connect("mongodb://0.0.0.0:27017/E-Library");
 const app = express();
+app.set('view engine', 'ejs');
 
 const registerSchema = {
     userName: { type: String, requried: true },
@@ -36,24 +38,32 @@ app.post('/register', (res,req) => {
 });
 
 app.post('/signup', (res, req) => {
-    // password = res.body.password;
     RegUser.findOne({email : res.body.email}, (err, data) => {
         if(err) { 
             throw err;
          }
         else {
             if(data === null) {
-                console.log("Invalid Username");
+                console.log("Invalid Email");
             }
-            else if(data.password === res.body.password) {
+            else if((data.password === res.body.password) && (data.email === res.body.email)) {
                 console.log("Login Success");
+                req.redirect('/user/dashboard');
             }
             else {
                 console.log("Invalid password");
             }
         }
     })
-    req.redirect('/');
+    // req.redirect('/');
 });
 
-app.listen(3000, () => { console.log("Server running on port 3000")});
+app.get('/user/dashboard', (req, res) => {
+    // res.sendFile(`${__dirname}/public/bookList.html`);
+    res.sendFile(`${__dirname}/public/dashboardAdmin.html`);
+});
+
+app.get('admin/home', (req, res) => {
+});
+
+app.listen(4000, () => { console.log("Server running on port 4000")});
